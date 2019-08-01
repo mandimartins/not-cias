ba5f5e18bb8330136223271df4dd5ca2d93e9b67
Amanda Martins <mandimartins23@gmail.com>
2019-08-01 07:32:10 -0300

    Iniciando projeto

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+const mongo = process.env.MONGODB || 'mongodb://localhost/noticias'
+
+app.set('views',path.join(__dirname,'views'))
+
+app.set('view engine','ejs')
+
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+mongoose.connect(mongo)
+.then(()=>{
+    app.listen(port, ()=>{
+        console.log('listening')
+    })
+})
+.catch((error)=>{
+    console.log(error)
+})
+
